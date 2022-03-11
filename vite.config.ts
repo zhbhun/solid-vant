4ca3393f6fcb0fc1a9bd32f95436db575fdd60af
zhbhun <zhbhun@gmail.com>
2022-03-11 17:58:29 +0800

    chore: support alias and sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
+  },
+  resolve: {
+    alias: {
+      "solid-vant": path.resolve(__dirname, "./src"),
+    },
   },
 });
